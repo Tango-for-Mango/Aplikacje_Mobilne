64b6ab45fda7c9b19223cd8039c6d00b0c2b2924
Tango-for-Mango <153934492+Tango-for-Mango@users.noreply.github.com>
2025-03-12 09:30:29 +0100

    zadanie 2

--- Main.kt
+++ Main.kt
@@ -1,18 +1,8 @@
-fun foo(n: Int) {
-    for (i in 1..n) {
-        var output = "" // output pusty, sama liczba
-
-        if (i % 3 == 0) output += "trzy"
-        if (i % 5 == 0) output += "piec"
-        if (i % 7 == 0) output += "siedem"
-        if (i % 11 == 0) output += "jedenascie"
-        if (i % 13 == 0) output += "trzynascie"
-
-        println(if (output.isEmpty()) i else output) // jesli liczba nie jest podzielna przez zadny z warunków, zwraca i -> tą liczbę;
-                                                    // w przeciwnym wypadku, zwraca stringa
-    }
+fun isPalindrome(word: String): Boolean {
+    return word == word.reversed() // obraca stringa i porównuje czy jest identyczny z początkowym
 }
 
 fun main() {
-    foo(15) // Przykładowe wywołanie
+    val word = readln() //pobieranie wyrazu od uzytkownika
+    println(isPalindrome(word)) // sprawdzane jest pobrane słowo
 }
